e4941f18925df2cf9ebbe3238e4f50ea06bdb53c
DOMINATORZXC <pekaface1488@gmail.com>
2024-08-14 17:04:04 +0300

    Commit TasksControl

--- DataAccess/obj/Debug/net6.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net6.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bcf4798aad4547ec99c525e0b9cc83ee5998876")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
